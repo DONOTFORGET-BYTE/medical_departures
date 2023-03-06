2f830208d85b8f76f09b20034d640f0709dff657
DONOTFORGET-BYTE <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-03-06 11:54:35 +0200

    patch: fix bugs

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -3,11 +3,11 @@ import connect from "../db/connect";
 var conn = connect();
 
 export function createUsersTable(){
-    var sql = "CREATE TABLE IF NOT EXISTS users(user_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,user_id int NOT NULL, email VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(255) not null)";
+    var sql = "CREATE TABLE IF NOT EXISTS users(user_id int NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(255) not null)";
 
     conn.query(sql, function (err: any, result: any) {
         if (err) throw err;
-        // console.log("users table created"); //testing purposes
+        console.log("users table created"); //testing purposes
     });
 
 }
@@ -16,7 +16,7 @@ export function createUsersProfileTable(){
     var sql = "CREATE TABLE IF NOT EXISTS user_profile(id int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,user_id int NOT NULL, name VARCHAR(255), surname VARCHAR(255), phone VARCHAR(15),FOREIGN KEY (user_id) REFERENCES users(user_id))"
     conn.query(sql, function (err: any, result: any) {
         if (err) throw err;
-        // console.log("users_profile table created"); //testing purposes
+        console.log("users_profile table created"); //testing purposes
     });
 
 }
