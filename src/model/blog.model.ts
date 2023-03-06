b9107f8ed385a80efdb888434da02587fd6d91c6
DONOTFORGET-BYTE <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-03-06 10:00:44 +0200

    feat: create blog model

---
+++ src/model/blog.model.ts
@@ -0,0 +1,15 @@
+import connect from "../db/connect";
+
+var conn = connect();
+
+function createBlogTable(){
+    var sql = "CREATE TABLE IF NOT EXISTS blog(blog_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,author int NOT NULL, title VARCHAR(50), content VARCHAR(255),FOREIGN KEY (author) REFERENCES users(user_id))";
+
+    conn.query(sql, function (err: any, result: any) {
+        if (err) throw err;
+        console.log("blog table created"); //testing purposes
+    });
+
+}
+
+export default createBlogTable;
